3700f64860c7411caea0be1a908e87b6f385310c
Markdiocre <mark.manuel.business@gmail.com>
2022-09-19 19:02:57 +0800

    Fix bulk email

--- public/app.js
+++ public/app.js
@@ -13,29 +13,56 @@ createApp({
       port:'',
       subject:'',
       from:'',
-      attachPath:''
+      attachPath:'',
+
+      //ListBox
+      cantInsertSingle: false,
+      newEmail: '',
+      selected: [],
+      options: []
     }
   },
   methods:{
     send(){
+      
+      let data = {
+        senderEmail: this.senderEmail,
+        senderPass: this.senderPass,
+        sendTo: this.sendTo,
+        messageBody: this.messageBody,
+        server: this.server,
+        port: this.port,
+        subject: this.subject,
+        from: this.from,
+        attachPath: this.attachPath
+      }
+      if(this.selected.length != 0){
+        data.sendTo = this.selected
+      }else{
+        data.sendTo = this.sendTo
+      }
+
       axios({
         method: 'post',
         url: 'http://localhost:3000/send_mail/',
-        data: {
-          senderEmail: this.senderEmail,
-          senderPass: this.senderPass,
-          sendTo: this.sendTo,
-          messageBody: this.messageBody,
-          server: this.server,
-          port: this.port,
-          subject: this.subject,
-          from: this.from,
-          attachPath: this.attachPath
-        }
+        data: data
       }).then((res)=>{
         this.message = res.data.message
         alert(this.message)
       });
+    },
+    addToList(){
+      this.options.push({value:this.newEmail})
+      this.newEmail = ''
+      this.sendTo = ''
+      this.cantInsertSingle = true
+      
+    },
+    deleteAll(){
+      this.options = []
+      this.selected = []
+      this.cantInsertSingle = false
     }
+
   }
 }).mount('#app')
